e7e61470d74a6c2d8b00fd6670ad7ecee115bd81
Vincent Smithers <vsmithers1087@gmail.com>
2021-11-24 10:40:52 -0500

    Update for BatchRequest and PlanetsRequest for modern concurrency

--- Sources/SwissEphemeris/Protocols/BatchRequest.swift
+++ Sources/SwissEphemeris/Protocols/BatchRequest.swift
@@ -10,14 +10,24 @@ import Foundation
 /// Models a request for batch calculations at a consistent
 /// date interval. If you are making hundreds of calculations at one time
 /// it is recommended to use these utility methods for increased performance
-/// and to avoid the undefined behavior that results from making numerous calculations
-/// concurrently on a single thread.
+/// and to avoid the undefined behavior that results from making a high number of
+/// calculations concurrently.
 public protocol BatchRequest {
     /// The type returned from the fetch request.
     associatedtype EphemerisItem
     /// The maximum amount of concurrent calculations. Exceed `478` at your own risk.
     var datesThreshold: Int { get }
+	
+	@available(macOS 12.0.0, *)
+	/// Fetches a collection of `EphemerisItem` for a time interval through a span of dates.
+	/// - Parameters:
+	///   - start: The beginning of the date range.
+	///   - end: The end of the date range.
+	///   - interval: The frequency in which an item is calculated.
+	/// - Returns: An array of `EphemerisItem`.
+	func fetch(start: Date, end: Date, interval: TimeInterval) async -> [EphemerisItem]
     
+	@available(*, deprecated, renamed: "fetch(start:end:interval:_:)")
     /// Fetches all of the `RequestType`s  between two dates.
     /// - Parameters:
     ///   - start: The starting date.
